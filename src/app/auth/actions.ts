ceef758035879ed9b653c0568ca3b57d83acf126
João Dias <joaoeduardodias123@gmail.com>
2025-11-14 11:01:17 -0300

    fix: add state url

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -1,8 +1,9 @@
 "use server"
-
-import { redirect } from "next/navigation"
+import { redirect } from "next/navigation";
+import crypto from "node:crypto";
 
 export async function signInWithGoogle() {
+  const state = crypto.randomBytes(16).toString("hex");
   const googleSignInUrl = new URL("https://accounts.google.com/o/oauth2/v2/auth")
 
   googleSignInUrl.searchParams.set("client_id", process.env.GOOGLE_OAUTH_CLIENT_ID!)
@@ -11,6 +12,7 @@ export async function signInWithGoogle() {
   googleSignInUrl.searchParams.set("scope", "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile")
   googleSignInUrl.searchParams.set("access_type", "offline")
   googleSignInUrl.searchParams.set("prompt", "consent")
-  console.log(googleSignInUrl.toString());
+  googleSignInUrl.searchParams.set("state", state)
+
   redirect(googleSignInUrl.toString())
 }
